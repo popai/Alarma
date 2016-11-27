1700375e873bb5835698e222ddc36f4c8c73cf6f
popai <popai@b.astral.ro>
2016-11-27 09:06:34 +0200

    first commit

---
+++ Alarma.h
@@ -0,0 +1,22 @@
+// Only modify this file to include
+// - function definitions (prototypes)
+// - include files
+// - extern variable definitions
+// In the appropriate section
+
+#ifndef _Alarma_H_
+#define _Alarma_H_
+#include "Arduino.h"
+//add your includes for the project Alarma here
+
+
+//end of add your includes here
+
+
+//add your function definitions for the project Alarma here
+
+
+
+
+//Do not add code below this line
+#endif /* _Alarma_H_ */
